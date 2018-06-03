bec116c8a096b33595b09c0285f9f5160474ddfc
Yair Shalev <yairshalev77@gmail.com>
2018-06-03 23:28:06 +0300

    first commit

---
+++ NewFileFun.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import random
+
+def save_new_file (finalfolderpath, filetosave):
+    i=1
+    locationdata = list()
+    with open(filetosave, 'r') as rf:
+    
+        for line in rf:
+            s = 'aaa',i,'.txt'
+            with open(str(finalfolderpath)+str(i)+'.txt', 'w') as wf:
+                wf.write(line)
+                locationdata.append(finalfolderpath+str(i))
+                i=i+1           
+    return locationdata
+
+
+
+
+data = list()
+data = save_new_file('Newf1\FileItem', 'testFILE.txt')
+
+
+#--------------------------------------------------------
+# the function takes the future location for the parts
+# and the name of the original file (who must be in the same folder as
+# the code) and returns a list of the locations of the parts.
